6c5a6533018423bb241f1ea28819ea61bc9745c5
memojtabazamani <memojtabazamani@gmail.com>
2021-08-05 15:29:52 +0430

    done:
    نصب فایل ها برای پروژه
    و ستاپ آن

---
+++ user-manage-app/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <>
+            Hello There
+        </>
+    )
+}
+
+export default App;
