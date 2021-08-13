f16ba9fb3fec6204678268a5ec4c21d8d774d975
mojtaba zamani <56557417+memojtabazamani@users.noreply.github.com>
2021-08-13 17:12:18 +0430

    قسمت فرم داینامیک به باگ خورده

--- user-manage-app/client/src/components/posts/CreatePost.js
+++ user-manage-app/client/src/components/posts/CreatePost.js
@@ -3,67 +3,26 @@ import ReactDOM from 'react-dom';
 
 import {connect} from 'react-redux';
 import {createPost} from '../../actions';
-
-import { Formik, Form, useField } from 'formik';
 import * as Yup from 'yup';
 
-const MyTextInput = ({ label, ...props }) => {
-    const [field, meta] = useField(props);
-    return (
-        <div className='field'>
-            <label htmlFor={props.id || props.name}>{label}</label>
-            <input {...field} {...props} />
-            {meta.touched && meta.error ? (
-                <div className="ui negative message">
-                    <div className="header">
-                        {meta.error}
-                    </div>
-                </div>
-            ) : null}
-        </div>
-    );
-};
+import PostForm from './PostForm';
 
-const com =  (props) => {
+
+const com = (props) => {
+    const onSubmitForm = () => {
+        // console.log(values);
+        // props.createPost(values);
+        console.log("submirted");
+    }
     return (
         <>
-            <Formik
-                initialValues={
-                    { title: "", description: ""  }
-                }
-                validationSchema={Yup.object({
-                    title: Yup.string().required("عنوان پست باید درج شود")
-                })}
-                onSubmit={(values) => {
-                    props.createPost(values);
-                }}
-            >
-                <Form className='ui form'>
-                    <MyTextInput
-                        label="عنوان"
-                        name="title"
-                        type="text"
-                        placeholder="...."
-                        className='ui center aligned segment'
-                    />
-
-                    <MyTextInput
-                        label="توضیحات"
-                        name="description"
-                        type="text"
-                        placeholder="...."
-                        className='ui center aligned segment'
-                    />
-
-                    <button className="ui green button" type="submit">تأیید اطلاعات</button>
-                </Form>
-            </Formik>
+            <PostForm handleMe={() => onSubmitForm}/>
         </>
     )
 }
 
 const mapStateToProps = (state) => {
-    return {posts: state.posts }
+    return {posts: state.posts}
 }
 
 export default connect(mapStateToProps, {
---
+++ user-manage-app/client/src/components/posts/PostForm.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Formik, Form, useField } from 'formik';
+import * as Yup from 'yup';
+
+const MyTextInput = ({ label, ...props }) => {
+    const [field, meta] = useField(props);
+    return (
+        <div className='field'>
+            <label htmlFor={props.id || props.name}>{label}</label>
+            <input {...field} {...props} />
+            {meta.touched && meta.error ? (
+                <div className="ui negative message">
+                    <div className="header">
+                        {meta.error}
+                    </div>
+                </div>
+            ) : null}
+        </div>
+    );
+};
+
+const com =  (props) => {
+    return (
+        <>
+            <Formik
+                initialValues={
+                    { title: "", description: ""  }
+                }
+                validationSchema={Yup.object({
+                    title: Yup.string().required("عنوان پست باید درج شود")
+                })}
+                onSubmit={(values) => {
+                    console.log(values);
+                    props.handleMe(values);
+                }}
+            >
+                <Form className='ui form'>
+                    <MyTextInput
+                        label="عنوان"
+                        name="title"
+                        type="text"
+                        placeholder="...."
+                        className='ui center aligned segment'
+                    />
+
+                    <MyTextInput
+                        label="توضیحات"
+                        name="description"
+                        type="text"
+                        placeholder="...."
+                        className='ui center aligned segment'
+                    />
+
+                    <button className="ui green button" type="submit">تأیید اطلاعات</button>
+                </Form>
+            </Formik>
+        </>
+    )
+}
+
+
+export default com;
