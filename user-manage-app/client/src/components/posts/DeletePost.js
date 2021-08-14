e066eefe34dc57b9eb5b2192930fbb0e9ea7dcab
memojtabazamani <memojtabazamani@gmail.com>
2021-08-14 12:31:51 +0430

    حذف پست

--- user-manage-app/client/src/components/posts/DeletePost.js
+++ user-manage-app/client/src/components/posts/DeletePost.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 
 import {connect} from 'react-redux';
-import {createPost} from '../../actions';
+import {deletePost, fetchPost} from '../../actions';
 
-const com =  (props) => {
+import Modal from '../utilities/Modal';
+
+const Com =  (props) => {
+    useEffect(() => {
+        props.fetchPost(props.match.params.id);
+    }, []);
+    const deletePostEvent = () => {
+        props.deletePost(props.match.params.id);
+    }
+
+    const actions = (
+        <div className="actions">
+            <div className="ui red button" onClick={(e) => deletePostEvent() }>حذف</div>
+            <div className="ui cancel button">بستن</div>
+        </div>
+    )
+
+
+    const renderContent = () => {
+        if(!props.post) {
+            return "آیا شما برای حذف این پست مطمئن هستید؟";
+        } else {
+            return `آیا شما برای حذف این پست مطمئن هستید؟ [${props.post.title}] `;
+        }
+    }
+
     return (
         <>
-            Delete Post
+        <Modal
+            title="حذف پست"
+            content={renderContent()}
+            actions={actions}
+            />
         </>
-    )
+)
 }
 
-export default connect(null, {
-    createPost
-})(com);
+const mapStateToProps = (state, ownProps) => {
+    return {
+        post: state.posts[ownProps.match.params.id]
+    }
+}
+
+export default connect(mapStateToProps, {
+    deletePost,
+    fetchPost
+})(Com);
