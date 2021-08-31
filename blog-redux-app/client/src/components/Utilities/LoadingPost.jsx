84a9f9b628db59d5f82503418abddf6ca40323de
mojtaba zamani <56557417+memojtabazamani@users.noreply.github.com>
2021-08-31 20:52:42 +0430

    Create LoadingPost.jsx

---
+++ blog-redux-app/client/src/components/Utilities/LoadingPost.jsx
@@ -0,0 +1,14 @@
+/*
+This Component Used When I to Show Loading!
+*/
+
+const LoadingPost = () => {
+    return (
+        <div className="ui segment">
+            <div className="ui active loader"></div>
+            <p></p>
+        </div>
+    )
+};
+
+export default LoadingPost;
