e16e034d7c4cf43e34763c4c010c836e5cc34be6
mojtaba zamani <56557417+memojtabazamani@users.noreply.github.com>
2021-08-31 21:02:12 +0430

    ساخت کامپوننت پیام

---
+++ blog-redux-app/client/src/components/Utilities/Message.jsx
@@ -0,0 +1,32 @@
+/*
+This Component For show message in app!
+
+PROPS:
+- Icon
+- Header
+- Content
+*/
+
+const Message = ({ icon, header, content }) => {
+    return (
+        <div className="ui icon message">
+            <i className={`${icon} icon`}></i>
+            <div className="content">
+                <div className="header">
+                    {header}
+                </div>
+                <p>
+                    {content}
+                </p>
+            </div>
+        </div>
+    )
+};
+
+Message.defaultProps = {
+    icon: "inbox",
+    header: "Some Message !",
+    content: "Get the best news in your e-mail every day."
+};
+
+export default Message;
