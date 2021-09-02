9e4f666c4dd5a04079f38799b4085bb9b959fc53
mojtaba zamani <56557417+memojtabazamani@users.noreply.github.com>
2021-09-02 20:42:07 +0430

    طراحی اکشن ها

---
+++ blog-redux-app/client/src/actions/index.js
@@ -0,0 +1,131 @@
+import {POSTS} from "../constants";
+
+/*
+START: POSTS ACTIONS
+*/
+
+// Fetch Posts Request Action
+export const fetchPostsRequest = () => {
+  return {
+      type: POSTS.FETCH_POSTS_REQUEST
+  }
+};
+
+// Success Fetch Posts Action
+export const fetchPostsSuccess = (posts) => {
+  return {
+      type: POSTS.FETCH_POSTS_SUCCESS,
+      posts
+  }
+};
+
+// Error Fetch Posts Action
+export const fetchPostsError = (error) => {
+    return {
+        type: POSTS.FETCH_POSTS_ERROR,
+        error
+    }
+};
+
+
+// Fetch Create Post Action
+export const createPostRequest = (values) => {
+    return {
+        type: POSTS.CREATE_POST_REQUEST,
+        values
+    }
+};
+
+// Create Post Success Action
+export const createPostSuccess = (post) => {
+  return {
+      type: POSTS.CREATE_POST_SUCCESS,
+      post
+  }
+};
+
+// Create Post Error Action
+export const createPostError = (error) => {
+    return {
+        type: POSTS.CREATE_POST_ERROR,
+        error
+    }
+};
+
+// Update Post Request
+export const updatePostRequest = (values, id) => {
+    return {
+        type: POSTS.UPDATE_POST_REQUEST,
+        values,
+        id
+    }
+};
+
+// Update Post Success
+export const updatePostSuccess = (post) => {
+    return {
+        type: POSTS.UPDATE_POST_SUCCESS,
+        post
+    }
+};
+
+// Update Post Error
+export const updatePostError = (error) => {
+    return {
+        type:POSTS.UPDATE_POST_ERROR,
+        error
+    }
+};
+
+// Delete Post Request
+export const deletePostRequest = (id) => {
+    return {
+        type: POSTS.DELETE_POST_REQUEST,
+        id
+    }
+};
+
+// Delete Post Success
+export const deletePostSuccess = (id) => {
+    return {
+        type: POSTS.DELETE_POST_SUCCESS,
+        id
+    }
+};
+
+// Delete Post Error
+export const deletePostError = (error) => {
+    return {
+        type: POSTS.DELETE_POST_ERROR,
+        error
+    }
+};
+
+// Fetch Post Request Action
+export const fetchPostRequest = (id) => {
+    return {
+        type: POSTS.FETCH_POST_REQUEST,
+        id
+    }
+};
+
+// Success Fetch Post Action
+export const fetchPostSuccess = (post) => {
+    return {
+        type: POSTS.FETCH_POST_SUCCESS,
+        post
+    }
+};
+
+// Error Fetch Post Action
+export const fetchPostError = (error) => {
+    return {
+        type: POSTS.FETCH_POST_ERROR,
+        error
+    }
+};
+
+
+/*
+END: POSTS ACTIONS
+*/
